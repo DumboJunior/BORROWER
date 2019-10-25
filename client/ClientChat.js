2230529f706b2f84574475592c143d3fb8da0d85
DumboJunior <chesb17@student.sdu.dk>
2019-10-25 16:18:34 +0200

    #4 Server side chat setup completed with some inconveniences such as private messages only shows up on the receiver side. \n #5 Client side chat setup completed with some inconveniences, such as private messages only shows up on receiver side. Missing: Connection to database

---
+++ client/ClientChat.js
@@ -0,0 +1,28 @@
+// Inspired by Lec06 example 07-better-chat-client.html
+window.addEventListener("load", () => {
+	let ws = new WebSocket("ws://localhost:8080/");
+	let userid = document.getElementById("userid");
+	let input = document.getElementById("input");
+	let button = document.getElementById("button");
+	let connect = document.getElementById("connect")
+
+	ws.open = ()=> {console.log("connected")};
+	ws.onclose = () => console.log("disconnected", e.reason);
+	ws.onerror = () => console.log("Something went wrong: ",e);
+	ws.onmessage = event => {
+		let json = JSON.parse(event.data);
+		let node = document.createTextNode(json.time + " " + json.userid + ": " + '"' + json.msg+ '"');
+		let div = document.createElement("div");
+		div.appendChild(node);
+		document.body.appendChild(div);
+	};
+	connect.addEventListener("click", () => {
+		// only at initial connection
+		// setup userid for the server
+		ws.send(JSON.stringify({type:"connection", userid:userid.value}));
+	});
+	button.addEventListener("click", () => {
+		ws.send(JSON.stringify({type:"msg", msg:input.value, userid:userid.value, toUser:toUser.value}));
+		input.value="";
+	});
+});
